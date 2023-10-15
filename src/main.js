f467719f5dfe4a60f841866704bb2549ed656861
ronaldortiz99 <72008660+ronaldortiz99@users.noreply.github.com>
2023-10-15 02:04:08 +0200

    a

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import BootstrapVue from 'bootstrap-vue'
 import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.js";
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
